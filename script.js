7118bb08b0c101ea768d4717091f4ce96a3a1256
az1zbek-882 <mariosafarbayev4713@gmail.com>
2025-01-22 22:32:23 +0500

    first commit

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
 const menuItems = document.querySelectorAll('.Header_row_Top-bottom-btn');
+const sliderTrack = document.querySelector(".Slider_wrap-track");
+const sliderBtnnext = document.querySelector(".Slider_wrap-Btns-btn-next");
+const sliderBtnprev = document.querySelector(".Slider_wrap-Btns-btn-prev");
+
+const sliderItemWidth = document.querySelector(
+  ".Slider_wrap-track-menu-item"
+).clientWidth;
+
+console.log(sliderTrack);
+
+let activeSliderItem = 0;
+function moveNext(n) {
+  sliderTrack.style.left = `-${n * sliderItemWidth}px`;
+}
+function movePrev(n) {
+    sliderTrack.style.left = `-${n * sliderItemWidth}px`;
+  }
+  
+  
+
+sliderBtnnext.addEventListener("click", function () {
+  if (activeSliderItem === 20) {
+    activeSliderItem = 0;
+  } else {
+    activeSliderItem++;
+  }
+  moveNext(activeSliderItem);
+});
+sliderBtnprev.addEventListener("click", function () {
+    if (activeSliderItem === 0) {
+        activeSliderItem = 20;
+      } else {
+        activeSliderItem--;
+      }
+    movePrev(activeSliderItem)
+  });
+
+
+  
 menuItems.forEach(item => {
   item.addEventListener('click', () => {
     // Hamma elementlardan "active" klassini olib tashlash
